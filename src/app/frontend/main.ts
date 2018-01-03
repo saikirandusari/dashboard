6c6c0fe543c9db1f3922b27db506a2ea47bcc4ee
Sebastian Florek <Sebastian.Florek@ts.fujitsu.com>
2018-01-03 13:31:04 +0100

    Initial build configuration

---
+++ src/app/frontend/main.ts
@@ -0,0 +1,14 @@
+import './polyfills';
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowser } from '@angular/platform-browser';
+
+import {AppModuleNgFactory} from './app/app.module.ngfactory';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)
+  .catch(err => console.log(err));
