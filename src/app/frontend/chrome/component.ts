ccdf93956aaf544cd7c287a6f091e95b1dbc0b59
Sebastian Florek <Sebastian.Florek@ts.fujitsu.com>
2018-01-03 15:56:59 +0100

    Add support for scss

--- src/app/frontend/chrome/component.ts
+++ src/app/frontend/chrome/component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MatIconRegistry} from "@angular/material";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'kd-chrome',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class ChromeComponent {
   title = 'app';
+
+  constructor(
+    @Inject(MatIconRegistry) iconRegistry: MatIconRegistry,
+    @Inject(DomSanitizer) sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('kd-logo',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/images/kubernetes-logo.svg'));
+  }
 
   getOverviewStateName() {
     return 'someName';
--- src/app/frontend/chrome/module.ts
+++ src/app/frontend/chrome/module.ts
@@ -3,29 +3,30 @@ import {ChromeComponent} from './component';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {
-    MatButtonModule, MatIconModule, MatIconRegistry, MatProgressSpinnerModule, MatToolbarModule,
-    MatTooltipModule
+  MatButtonModule, MatIconModule, MatIconRegistry, MatProgressSpinnerModule, MatToolbarModule,
+  MatTooltipModule
 } from '@angular/material';
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {HttpClientModule} from "@angular/common/http";
 import {PipesModule} from "../common/pipes/module";
 
-
 @NgModule({
-    declarations: [
-        ChromeComponent
-    ],
-    imports: [
-        BrowserModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatTooltipModule,
-        MatButtonModule,
-        MatProgressSpinnerModule,
-        FlexLayoutModule,
-        PipesModule
-    ],
-    providers: [MatIconRegistry],
-    bootstrap: [ChromeComponent]
+  declarations: [
+    ChromeComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    PipesModule
+  ],
+  providers: [MatIconRegistry],
+  bootstrap: [ChromeComponent]
 })
 export class ChromeModule {
 }
