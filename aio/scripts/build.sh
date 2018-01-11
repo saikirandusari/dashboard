9d938fb8f53ebdda7bde0ba2da48123856baf575
Sebastian Florek <Sebastian.Florek@ts.fujitsu.com>
2018-01-11 11:59:35 +0100

    Adapt build dir similar to angular repository

--- aio/scripts/build.sh
+++ aio/scripts/build.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
+# Copyright 2017 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Exit on error
 set -e
 
-# ---------- Define constants ---------- #
+# ---------- Import config ---------- #
 
-BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT_DIR=$(dirname "BUILD_DIR")
-I18N_DIR=${ROOT_DIR}/i18n
-NG_BIN=${ROOT_DIR}/node_modules/.bin/ng
-GULP_BIN=${ROOT_DIR}/node_modules/.bin/gulp
-TMP_DIR=${ROOT_DIR}/.tmp
-FRONTEND_DIR=${TMP_DIR}/frontend
-DIST_DIR=${ROOT_DIR}/dist
+ROOT_DIR="$(cd $(dirname "${BASH_SOURCE}")/../.. && pwd -P)"
+source "${ROOT_DIR}/aio/scripts/conf.sh"
 
 # ---------- Define variables ---------- #
 
