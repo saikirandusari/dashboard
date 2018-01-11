d06888adf669de554e6067a908bbbd83a35880e5
maciaszczykm <maciaszczykm@icloud.com>
2018-01-11 13:23:01 +0100

    Refactor script starting cluster

--- aio/scripts/conf.sh
+++ aio/scripts/conf.sh
@@ -35,3 +35,12 @@ HEAPSTER_PORT=8082
 MINIKUBE_VERSION=v0.24.1
 MINIKUBE_K8S_VERSION=v1.8.0
 MINIKUBE_BIN=${CACHE_DIR}/minikube-${MINIKUBE_VERSION}
+
+# Setup logger.
+ERROR_STYLE=`tput setaf 1`
+INFO_STYLE=`tput setaf 2`
+BOLD_STYLE=`tput bold`
+RESET_STYLE=`tput sgr0`
+
+function log-info { echo -e "\n${INFO_STYLE}${BOLD_STYLE}$@${RESET_STYLE}"; }
+function log-error { echo -e "\n${ERROR_STYLE}${BOLD_STYLE}$@${RESET_STYLE}"; }
