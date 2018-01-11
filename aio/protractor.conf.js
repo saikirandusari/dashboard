45568e91ace19a6bde3418349a30935c37c46bb6
Sebastian Florek <Sebastian.Florek@ts.fujitsu.com>
2018-01-11 12:33:58 +0100

    Move tsconfig files out of src dir

--- aio/protractor.conf.js
+++ aio/protractor.conf.js
@@ -5,14 +5,14 @@ const {SpecReporter} = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: ['./e2e/**/*.e2e-spec.ts'],
+  specs: ['../src/test/e2e/**/*.e2e-spec.ts'],
   capabilities: {'browserName': 'chrome'},
   directConnect: true,
   baseUrl: 'http://localhost:8080/',
   framework: 'jasmine',
   jasmineNodeOpts: {showColors: true, defaultTimeoutInterval: 30000, print: function() {}},
   onPrepare() {
-    require('ts-node').register({project: 'e2e/tsconfig.e2e.json'});
+    require('ts-node').register({project: 'aio/tsconfig.e2e.json'});
     jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
   }
 };
